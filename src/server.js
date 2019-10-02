8bf0d95e324172e5e0007293e015339a496e3aa1
jcagz96 <jcagz@iscte-iul.pt>
2019-10-02 19:56:29 +0100

    added cors package

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const app = express();
 
@@ -22,7 +23,7 @@ mongoose.connect(process.env.DB_CONNECT,
 //req.params = Acessar route params   , ex: (http://localhost:3333/users/1)
 //req.body = Acessar corpo da requisição , ex: {"nome" : "Joao"}
 
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
