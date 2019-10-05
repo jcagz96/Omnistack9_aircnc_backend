fad6dea71dd534eeac6d2891e4788c1257629215
jcagz96 <jcagz@iscte-iul.pt>
2019-10-05 11:10:28 +0100

    added approval & rejection controllers, routes & added websockets with socketio

--- src/server.js
+++ src/server.js
@@ -5,8 +5,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
 
 mongoose.connect(process.env.DB_CONNECT,
     {
@@ -18,6 +24,23 @@ mongoose.connect(process.env.DB_CONNECT,
         console.log(`Database connected `);
     }
 );
+
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
 
 //GET,  POST,   PUT,   DELETE
 
@@ -31,6 +54,6 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
     console.log(`Server is running... Port: ${process.env.PORT}`);
 });
