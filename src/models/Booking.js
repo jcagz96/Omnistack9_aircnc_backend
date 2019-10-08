3ac21b5c9ce86077dac58b2f5be09d8b5aa3be4e
jcagz96 <jcagz@iscte-iul.pt>
2019-10-08 19:28:00 +0100

    added formated_date to Booking model

--- src/models/Booking.js
+++ src/models/Booking.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const BookingSchema = new mongoose.Schema({
-    date: String,
+    date: {
+        type: Date,
+    },
     approved: Boolean,
     user: {
         type: mongoose.Schema.Types.ObjectId,
@@ -11,6 +14,14 @@ const BookingSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Spot'
     }
+}, {
+    toJSON: {
+        virtuals: true,
+    }
 });
+
+BookingSchema.virtual('formated_date').get(function () {
+    return moment(this.date).format('DD-MM-YYYY h:mm:ss A');
+})
 
 module.exports = mongoose.model('Booking', BookingSchema);
